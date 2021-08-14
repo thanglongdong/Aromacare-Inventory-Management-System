92ad4d3271ec2cb1ad4acd4349579ba58f7d6e01
aimor2 <aimor2@student.monash.edu>
2021-08-15 01:01:21 +1000

    angusdunno

--- templates/Suppliers/index.php
+++ templates/Suppliers/index.php
@@ -3,7 +3,56 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Supplier[]|\Cake\Collection\CollectionInterface $suppliers
  */
+
+echo $this -> Html->css("/vendor/datatables/dataTables.bootstrap4.min.css",['block'=>true]);
+echo $this -> Html->script("/vendor/datatables/jquery.dataTables.min.js",['block'=>true]);
+echo $this -> Html->script("/vendor/datatables/dataTables.bootstrap4.min.js",['block'=>true]);
+echo $this -> Html->script("/js/demo/datatables-demo.js",['block'=>true]);
+
 ?>
+
+<!-- Tabs -->
+<?php $page_name = $this->request->getparam("controller") ?>
+<p></p>
+<!-- End of Tabs -->
+
+<div>
+    <div class="mb-3 d-sm-flex align-items-center justify-content-between mb-4">
+        <h3 class="text-grey"><?= __('Suppliers') ?></h3>
+        <a href="<?= $this->Url->build('/suppliers/add')?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-plus fa-sm text-white-50"></i> Add Supplier</a>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+            <tr>
+                <th><?= h('ID') ?></th>
+                <th><?= h('Name') ?></th>
+                <th><?= h('Phone') ?></th>
+                <th><?= h('Email') ?></th>
+                <th class="actions"><?= __('Actions') ?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($suppliers as $supplier): ?>
+                <tr>
+                    <td><?= $this->Number->format($supplier->id) ?></td>
+                    <td><?= h($supplier->name) ?></td>
+                    <td><?= h($supplier->phone) ?></td>
+                    <td><?= h($supplier->email) ?></td>
+                    <td class="actions">
+                        <?= $this->Html->link(__('View'), ['action' => 'view', $supplier->id]) ?>
+                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $supplier->id]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $supplier->id], ['confirm' => __('Are you sure you want to delete this supplier {0}?', $supplier->name)]) ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<!--
 <div class="suppliers index content">
     <?= $this->Html->link(__('New Supplier'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Suppliers') ?></h3>
@@ -45,4 +94,4 @@
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
-</div>
+</div> -->
