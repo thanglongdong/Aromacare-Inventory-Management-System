240977c7d76707a588bc6c04540e7ff297210bf0
aimor2 <aimor2@student.monash.edu>
2021-08-15 13:29:30 +1000

    supplieradd

--- templates/Suppliers/add.php
+++ templates/Suppliers/add.php
@@ -4,6 +4,50 @@
  * @var \App\Model\Entity\Supplier $supplier
  */
 ?>
+
+<!-- Tabs -->
+<?php $page_name = $this->request->getparam("controller") ?>
+<p></p>
+<!-- End of Tabs -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div>
+                <h1 class="h3 mb-2 text-gray-800"><?= __('New Supplier') ?></h1>
+            </div>
+
+
+            <div>
+                <?= $this->Form->create($supplier,['novalidate' => true, 'type'=>'file']) ?>
+                <?= $this->Form->control('name') ?>
+                <!-- Row 1 -->
+                <div class="row">
+                    <div class="col">
+                        <?= $this->Form->control('Phone Number') ?>
+                    </div>
+                    <div class="col">
+                        <?= $this->Form->control('Email') ?>
+                    </div>
+                    <div class="col">
+                        <?= $this->Form->control('Address'); ?>
+                    </div>
+                </div>
+
+                <div>
+                    <?= $this->Form->button(__('Add Supplier'), ['class' => 'btn btn-primary']) ?>
+                    <?= $this->Html->link(__('List Suppliers'), ['action' => 'index'], ['class' => 'btn btn-outline-primary me-2 float-right mr-2']) ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+<?= $this->Form->end() ?>
+<br </br>
+
+<!-- this is all the old stuff that was there before
 <div class="row">
     <aside class="column">
         <div class="side-nav">
@@ -27,4 +71,4 @@
             <?= $this->Form->end() ?>
         </div>
     </div>
-</div>
+</div> -->
