c1891009683d417decee3ff3d7abe7a1970c17e0
1225xwen <xwen0017@student.monash.edu>
2021-08-19 11:20:21 +0800

    display supplier name & update error/success message

--- templates/element/flash/success.php
+++ templates/element/flash/success.php
@@ -8,4 +8,4 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message success" onclick="this.classList.add('hidden')"><?= $message ?></div>
+<div class="message success" style="color:#198754;font-size:14px;text-align:center;font-weight:bold"  onclick="this.classList.add('hidden')"><?= $message ?></div>
