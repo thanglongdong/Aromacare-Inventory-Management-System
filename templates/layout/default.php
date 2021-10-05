97558af5f7a87ea689268bbaaf105ca38afa5554
mcol0018 <mcol0018@student.monash.edu>
2021-10-05 18:05:43 +1100

    final fix of datatables & re-add /vendor to gitignore

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -13,6 +13,7 @@
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Core theme CSS (includes Bootstrap)-->
