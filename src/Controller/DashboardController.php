ff811791c07ada775d2a15a252ca1f084a136156
mcol0018 <mcol0018@student.monash.edu>
2021-10-07 15:33:11 +1100

    fixed possible canvas js error from dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -78,6 +78,7 @@ class DashboardController extends AppController
         $list = array();
         for ($i=0; $i<10; $i++) {
             $newNumber = 999;
+            $lowestID = -1;
             foreach ($ingredientsEntriesCopy as $ingredient) {
                 $number = $ingredient->stock - $ingredient->threshold;
 
@@ -104,19 +105,25 @@ class DashboardController extends AppController
             }
 
         }
+        $dataPoints1 = array();
 
-        $dataPoints1= array(
-            array("y" => $list[18], "label"=> $list[19] ),
-            array("y" => $list[16], "label"=> $list[17] ),
-            array("y" => $list[14], "label"=> $list[15] ),
-            array("y" => $list[12], "label"=> $list[13] ),
-            array("y" => $list[10], "label"=> $list[11] ),
-            array("y" => $list[8], "label"=> $list[9] ),
-            array("y" => $list[6], "label"=> $list[7]),
-            array("y" => $list[4], "label"=> $list[5] ),
-            array("y" => $list[2], "label"=> $list[3] ),
-            array("y" => $list[0], "label"=> $list[1] )
-        );
+        for ($i=0;$i<count($list);) {
+            array_unshift($dataPoints1,array("y" => $list[$i], "label"=> $list[$i+1]));
+            $i = $i + 2;
+        }
+
+//        $dataPoints1= array(
+//            array("y" => $list[18], "label"=> $list[19] ),
+//            array("y" => $list[16], "label"=> $list[17] ),
+//            array("y" => $list[14], "label"=> $list[15] ),
+//            array("y" => $list[12], "label"=> $list[13] ),
+//            array("y" => $list[10], "label"=> $list[11] ),
+//            array("y" => $list[8], "label"=> $list[9] ),
+//            array("y" => $list[6], "label"=> $list[7]),
+//            array("y" => $list[4], "label"=> $list[5] ),
+//            array("y" => $list[2], "label"=> $list[3] ),
+//            array("y" => $list[0], "label"=> $list[1] )
+//        );
 
 
         $this->set('ingredients', $ingredientsEntries);
